95653c982a729a4b918d69e6444be534ffb78616
Arkadiusz Stanisław Łęga <arkadiusz.stanislaw.lega@gmail.com>
2021-04-15 19:47:10 +0200

    Update controller mag app - fixed adding temp and hum on one graph

--- MagApp/controller.py
+++ MagApp/controller.py
@@ -4,6 +4,9 @@ from sqlalchemy import desc
 
 
 class MagAppController:
+    STRING_LABEL_TEMPERATURE = " temperatura"
+    STRING_LABEL_HUMIDITY = " wilgotność"
+
     def __init__(self, model, view):
         self.__model = model
         self.__view = view
@@ -16,7 +19,7 @@ class MagAppController:
 
         times = []
         labels = {}
-        temp = {}
+        values = {}
 
         # 4: DigitalReading.query.filter(DigitalReading.sensor_id == 4).limit(100).all()
 
@@ -30,13 +33,41 @@ class MagAppController:
         for i in sensors.get(1):
             times.append(i.time)
 
-        for i in sensors:
-            labels[i] = i
-            temp[i] = []
-            for x in sensors.get(i):
-                if self.__view.is_humidity_selected.get():
-                    temp.get(i).append(x.humidity)
-                elif self.__view.is_temperature_selected.get():
-                    temp.get(i).append(x.temperature)
+        is_temp = self.__view.is_temperature_selected.get()
+        is_hum = self.__view.is_humidity_selected.get()
 
-        self.__view.show_graph(times=times, data=temp, labels=labels)
+        for i in sensors:
+
+            hum_key = i
+            temp_key = i
+
+            if is_hum and not is_temp:
+                labels[i] = i
+                values[hum_key] = []
+
+            elif is_temp and not is_hum:
+                labels[i] = i
+                values[temp_key] = []
+
+            elif is_hum and is_temp:
+                hum_key = str(i) + self.STRING_LABEL_HUMIDITY
+                temp_key = str(i) + self.STRING_LABEL_TEMPERATURE
+
+                labels[hum_key] = hum_key
+                labels[temp_key] = temp_key
+
+                values[hum_key] = []
+                values[temp_key] = []
+
+            for x in sensors.get(i):
+                if self.__view.is_humidity_selected.get() and not self.__view.is_temperature_selected.get():
+                    values.get(hum_key).append(x.humidity)
+
+                elif self.__view.is_temperature_selected.get() and not self.__view.is_humidity_selected.get():
+                    values.get(temp_key).append(x.temperature)
+
+                elif self.__view.is_humidity_selected.get() and self.__view.is_temperature_selected.get():
+                    values.get(hum_key).append(x.humidity)
+                    values.get(temp_key).append(x.temperature)
+
+        self.__view.show_graph(times=times, data=values, labels=labels)
