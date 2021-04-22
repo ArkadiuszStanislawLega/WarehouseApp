be1c9a7317d8c56ca6993fc5eb4b7f5269c8a899
ArkadiuszStanislaLega <arkadiusz.stanislaw.lega@gmail.com>
2021-04-22 11:22:38 +0200

    Update device view - added constatnt strings.

--- Fragments/add_device_view.py
+++ Fragments/add_device_view.py
@@ -2,15 +2,19 @@ from tkinter import LabelFrame, Label, Widget, Button, Frame, W, BOTH, YES, LEFT
 
 
 class AddDeviceView (Widget):
+    STRING_ADD_DEVICE = "Dodaj urządzenie"
+    STRING_DEVICE_NAME = "Nazwa urządzenia:"
+    STRING_CHOSE_WAREHOUSE = "Wybierz magazyn:"
+    
     def __init__(self, master=None, cnf={}, **k):
         self.__lf_add_device = LabelFrame(master,
-                                          text="Dodaj urządzenie",
+                                          text=self.STRING_ADD_DEVICE,
                                           padx=10,
                                           pady=10)
         self.__lf_add_device.pack(anchor=W, expand=YES, fill=BOTH)
 
         self.__l_add_device_set_name = Label(self.__lf_add_device,
-                                             text="Nazwa urządzenia:")
+                                             text=self.STRING_DEVICE_NAME)
 
         self.__l_add_device_set_name.grid(row=0, column=0)
         self.__e_add_device = Entry(self.__lf_add_device)
@@ -21,11 +25,11 @@ class AddDeviceView (Widget):
         self.__om_select_warehouse = None
 
         self.__l_add_device_select_warehouse = Label(self.__lf_add_device,
-                                                     text="Wybierz magazyn:")
+                                                     text=self.STRING_CHOSE_WAREHOUSE)
         self.__l_add_device_select_warehouse.grid(row=0, column=2)
 
         self.__b_add_device = Button(self.__lf_add_device,
-                                     text="Dodaj urządzenie")
+                                     text=self.STRING_ADD_DEVICE)
         self.__b_add_device.grid(row=1, column=0)
 
     @property
