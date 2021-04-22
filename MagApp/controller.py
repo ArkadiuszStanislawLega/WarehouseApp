7ad5b5c7415a4bae887acbd8dc1f3efab07d4acd
ArkadiuszStanislaLega <arkadiusz.stanislaw.lega@gmail.com>
2021-04-22 13:37:50 +0200

    Update model  - added creating graphs.

--- MagApp/controller.py
+++ MagApp/controller.py
@@ -5,9 +5,6 @@ import datetime
 
 
 class MagAppController:
-    STRING_LABEL_TEMPERATURE = " temperatura"
-    STRING_LABEL_HUMIDITY = " wilgotność"
-
 
     def __init__(self, model, view):
         self.__model = model
@@ -34,104 +31,41 @@ class MagAppController:
     def refresh_table(self):
         self.__view.right_section.refresh(self.__model.data_for_table())
 
-    def __prepare_data_from_db(self, from_date=None, to_date=None):
-        full_data = {}
-        filtered = {}
-        for i in self.__view.right_section.table.selection():
-            ids = int(self.__view.right_section.table.item(i, 'tag')[0])
-
-            full_data[ids] = DigitalReading.query.filter(
-                DigitalReading.sensor_id == ids).all()
-
-            if from_date:
-                filtered[ids] = []
-                for sensor in full_data[ids]:
-                    if sensor.time >= from_date and sensor.time <= to_date:
-                        filtered.get(ids).append(sensor)
-
-        if from_date:
-            return filtered
-        else:
-            return full_data
-
-    def __create_readings(self, sensors, temperature=False):
-        values = {}
-        for sensor in sensors:
-            temp_key = sensor
-            self.__labels[sensor] = sensor
-            values[temp_key] = []
-            for digital_read in sensors.get(sensor):
-                if temperature:
-                    values.get(temp_key).append(digital_read.temperature)
-                else:
-                    values.get(temp_key).append(digital_read.humidity)
-
-        return values
-
-    def __create_temp_and_humidity(self, sensors):
-        values = {}
-        for sensor in sensors:
-            hum_key = str(sensor) + self.STRING_LABEL_HUMIDITY
-            temp_key = str(sensor) + self.STRING_LABEL_TEMPERATURE
-
-            self.__labels[hum_key] = hum_key
-            self.__labels[temp_key] = temp_key
-
-            values[hum_key] = []
-            values[temp_key] = []
-
-            for digital_read in sensors.get(sensor):
-                values.get(hum_key).append(digital_read.humidity)
-                values.get(temp_key).append(digital_read.temperature)
-
-        return values
-
     def create_graph(self):
-        self.__labels = {}
-        sensors = {}
-        times = []
+        # Flagi wskazujące czy wykres ma być złożony z temperatury
+        # i wilgoci, czy tylko jeden parametrów jest wybrany.
+        is_temp = self.__view.left_section.create_graph_view.is_temperature_selected.get()
+        is_hum = self.__view.left_section.create_graph_view.is_humidity_selected.get()
 
         from_date = self.__view.left_section.create_graph_view.get_from_date()
         to_date = self.__view.left_section.create_graph_view.get_to_date()
 
-        if from_date and to_date:
-            sensors = self.__prepare_data_from_db(from_date=from_date,
-                                                  to_date=to_date)
-        else:
-            sensors = self.__prepare_data_from_db()
-
-        if len(sensors) > 0:
-            for i in sensors.keys():
-                # Upewnianie się że mamy jednakowy dystans czasowy.
-                # Tworzę tabele z czasami które były zbierane w określonym czasie.
-                for digital_read in sensors.get(i):
-                    times.append(digital_read.time)
-
-                break
-
-            # Flagi wskazujące czy wykres ma być złożony z temperatury
-            # i wilgoci, czy tylko jeden parametrów jest wybrany.
-            is_temp = self.__view.left_section.create_graph_view.is_temperature_selected.get()
-            is_hum = self.__view.left_section.create_graph_view.is_humidity_selected.get()
-
-            # Wyświetlenie wilgotności
-            if is_hum and not is_temp:
-                self.__view.left_section.create_graph_view.show_graph(times=times,
-                                                                      data=self.__create_readings(
-                                                                          sensors),
-                                                                      labels=self.__labels)
-            # Wyświetlenie temperatury
-            elif is_temp and not is_hum:
-                self.__view.left_section.create_graph_view.show_graph(times=times,
-                                                                      data=self.__create_readings(sensors,
-                                                                                                  temperature=True),
-                                                                      labels=self.__labels)
-            # Wyświetelnie temperatury i wilgotności
-            elif is_hum and is_temp:
-                data = self.__create_temp_and_humidity(sensors)
-                self.__view.left_section.create_graph_view.show_graph(times=times,
-                                                                      data=data,
-                                                                      labels=self.__labels)
+        # Wyświetlenie wilgotności
+        if is_hum and not is_temp:
+            self.__model.create_graph(from_date=from_date, 
+                                      to_date=to_date,
+                                      selected=self.__view.right_section.table)
+            self.__view.left_section.create_graph_view.show_graph(times=self.__model.times,
+                                                                  data=self.__model.graph_values,
+                                                                  labels=self.__model.labels)
+        # Wyświetlenie temperatury
+        elif is_temp and not is_hum:
+            self.__model.create_graph(from_date=from_date, 
+                                      to_date=to_date, 
+                                      selected=self.__view.right_section.table, 
+                                      temperature=True)
+            self.__view.left_section.create_graph_view.show_graph(times=self.__model.times,
+                                                                      data=self.__model.graph_values,
+                                                                      labels=self.__model.labels)
+        # Wyświetelnie temperatury i wilgotności
+        elif is_hum and is_temp:
+            self.__model.create_graph(from_date=from_date, 
+                                      to_date=to_date, 
+                                      selected=self.__view.right_section.table, 
+                                      both=True)
+            self.__view.left_section.create_graph_view.show_graph(times=self.__model.times,
+                                                                  data=self.__model.graph_values,
+                                                                  labels=self.__model.labels)
 
     def edit_sensor(self):
         self.__view.left_section.sensor_detail_view.switch_edit_mode()
--- MagApp/model.py
+++ MagApp/model.py
@@ -6,9 +6,25 @@ from sqlalchemy import desc
 class MagAppModel:
     STRING_EMPTY_VALUE = "--"
     FORMAT_DATE = "%d-%m-%y %H:%M:%S"
+    STRING_LABEL_TEMPERATURE = " temperatura"
+    STRING_LABEL_HUMIDITY = " wilgotność"
 
     def __init__(self):
-        pass
+        self.__labels = {}
+        self.__times = []
+        self.__graph_values = {}
+
+    @property
+    def labels(self):
+        return self.__labels
+
+    @property
+    def times(self):
+        return self.__times
+
+    @property
+    def graph_values(self):
+        return self.__graph_values
     
     def data_for_table(self):
         w = Warehouse()
@@ -46,3 +62,83 @@ class MagAppModel:
                                         self.STRING_EMPTY_VALUE)
 
         return values
+
+    def __prepare_data_from_db(self, selected, from_date=None, to_date=None):
+        full_data = {}
+        filtered = {}
+        for i in selected.selection():
+            ids = int(selected.item(i, 'tag')[0])
+
+            full_data[ids] = DigitalReading.query.filter(
+                DigitalReading.sensor_id == ids).all()
+
+            if from_date:
+                filtered[ids] = []
+                for sensor in full_data[ids]:
+                    if sensor.time >= from_date and sensor.time <= to_date:
+                        filtered.get(ids).append(sensor)
+
+        if from_date:
+            return filtered
+        else:
+            return full_data
+
+    def __create_readings(self, sensors, temperature=False):
+        self.__graph_values.clear()
+        self.__labels.clear()
+        for sensor in sensors:
+            temp_key = sensor
+            self.__labels[sensor] = sensor
+            self.__graph_values[temp_key] = []
+            for digital_read in sensors.get(sensor):
+                if temperature:
+                    self.__graph_values.get(temp_key).append(digital_read.temperature)
+                else:
+                    self.__graph_values.get(temp_key).append(digital_read.humidity)
+
+        return self.__graph_values
+
+    def __create_temp_and_humidity(self, sensors):
+        self.__graph_values.clear()
+        self.__labels.clear()
+        for sensor in sensors:
+            hum_key = str(sensor) + self.STRING_LABEL_HUMIDITY
+            temp_key = str(sensor) + self.STRING_LABEL_TEMPERATURE
+
+            self.__labels[hum_key] = hum_key
+            self.__labels[temp_key] = temp_key
+
+            self.__graph_values[hum_key] = []
+            self.__graph_values[temp_key] = []
+
+            for digital_read in sensors.get(sensor):
+                self.__graph_values.get(hum_key).append(digital_read.humidity)
+                self.__graph_values.get(temp_key).append(digital_read.temperature)
+
+        return self.__graph_values
+
+    def create_graph(self, from_date, to_date, selected, temperature= False, both=False):
+ 
+        values = {}
+        self.__times.clear()
+
+        if from_date and to_date:
+            values = self.__prepare_data_from_db(from_date=from_date, 
+                                                 to_date=to_date,
+                                                 selected=selected)
+        else:
+            values = self.__prepare_data_from_db(selected=selected)
+
+        if len(values) > 0:
+            for i in values.keys():
+                # Upewnianie się że mamy jednakowy dystans czasowy.
+                # Tworzę tabele z czasami które były zbierane w określonym czasie.
+                for digital_read in values.get(i):
+                    self.__times.append(digital_read.time)
+
+                break
+
+        if not both:
+            self.__create_readings(values, temperature)
+        else:
+            self.__create_temp_and_humidity(values)
