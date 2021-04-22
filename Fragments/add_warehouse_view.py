559583b8a8b0154b654daff43a2def91599b0b3d
ArkadiuszStanislaLega <arkadiusz.stanislaw.lega@gmail.com>
2021-04-22 11:26:21 +0200

    Update add warehouse view - added constant strings

--- Fragments/add_warehouse_view.py
+++ Fragments/add_warehouse_view.py
@@ -2,20 +2,23 @@ from tkinter import LabelFrame, Label, Widget, Button, BOTH, YES, LEFT, Entry, W
 
 
 class AddWarehouseView (Widget):
+    STRING_ADD_WAREHOUSE = "Dodaj magazyn"
+    STRING_WAREHOUSE_NAME = "Nazwa magazynu:"
+
     def __init__(self, master=None, cnf={}, **k):
         self.__lf_add_warehouse = LabelFrame(master,
-                                             text="Dodaj magazyn",
+                                             text=self.STRING_ADD_WAREHOUSE,
                                              padx=10,
                                              pady=10)
         self.__lf_add_warehouse.pack(anchor=W, expand=YES, fill=BOTH)
         self.__l_add_warehouse_set_name = Label(self.__lf_add_warehouse,
-                                                text="Nazwa magazynu:")
+                                                text=self.STRING_WAREHOUSE_NAME)
         self.__l_add_warehouse_set_name.pack(side=LEFT)
         self.__e_add_warehouse = Entry(self.__lf_add_warehouse)
         self.__e_add_warehouse.pack(side=LEFT)
 
         self.__b_add_warehouse = Button(self.__lf_add_warehouse,
-                                        text="Dodaj magazyn")
+                                        text=self.STRING_ADD_WAREHOUSE)
         self.__b_add_warehouse.pack(expand=YES, fill=BOTH)
 
     @property
